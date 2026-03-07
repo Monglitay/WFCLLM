df6b05b2ad8ebdd56c287bad45d380980b3a3739
monglitay <monglitay@users.noreply.github.com>
2026-03-07 22:23:22 +0800

    chore: update dependencies and encoder package exports

---
+++ wfcllm/encoder/__init__.py
@@ -0,0 +1,6 @@
+"""Semantic encoder pretraining module."""
+
+from wfcllm.encoder.config import EncoderConfig
+from wfcllm.encoder.model import SemanticEncoder
+
+__all__ = ["EncoderConfig", "SemanticEncoder"]
