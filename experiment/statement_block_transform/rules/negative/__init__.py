c29ee28fc54a0d97dfec14811a85bd9bc459d67c
monglitay <monglitay@users.noreply.github.com>
2026-03-07 20:08:03 +0800

    feat: scaffold negative rules directory with empty registry

---
+++ experiment/statement_block_transform/rules/negative/__init__.py
@@ -0,0 +1,8 @@
+"""Negative transform rules registry."""
+
+from rules.base import Rule
+
+
+def get_all_negative_rules() -> list[Rule]:
+    """Return all negative transformation rules."""
+    return []
