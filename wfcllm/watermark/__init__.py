feb3aadcc404a823dc57050b948bbee4dfdbca7f
Monglitay <2811924088@qq.com>
2026-05-15 21:47:35 +0800

    refactor: route wfcllm.watermark public API through orchestrator canonical path

--- wfcllm/watermark/__init__.py
+++ wfcllm/watermark/__init__.py
@@ -2,7 +2,7 @@
 
 from wfcllm.watermark.config import WatermarkConfig
 from wfcllm.watermark.adaptive_gamma.entropy import NodeEntropyEstimator
-from wfcllm.watermark.generator import GenerateResult, WatermarkGenerator
+from wfcllm.watermark.orchestrator import GenerateResult, WatermarkGenerator
 from wfcllm.watermark.interceptor import InterceptEvent, StatementInterceptor
 from wfcllm.watermark.keying import WatermarkKeying
 from wfcllm.watermark.kv_cache import CacheSnapshot, KVCacheManager
